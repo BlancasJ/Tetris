e8d3645b47920b343fe1d4e257b2ea8a020645fb
Jorge Blancas <color.ui>
2022-08-20 10:11:50 -0500

    Add project files.

---
+++ Tetris/GameGrid.cs
@@ -0,0 +1,98 @@
+﻿namespace Tetris
+{
+    public class GameGrid
+    {
+        private readonly int[,] grid;
+        public int Rows { get; }
+        public int Columns { get; }
+
+        public int this[int row, int column]
+        {
+            get => grid[row, column];
+            set => grid[row, column] = value;
+        }
+
+        public GameGrid(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            grid = new int[rows, columns];
+        }
+
+        public bool IsInside(int row, int column)
+        {
+            bool isRowInside = row >= 0 && row < Rows;
+            bool isColumnInside = column >= 0 && column < Columns;
+
+            return isRowInside && isColumnInside;
+        }
+
+        public bool IsEmpty(int row, int column)
+        {
+            return IsInside(row, column) && grid[row, column] == 0;
+        }
+
+        public bool IsRowFull(int row)
+        {
+            for (int column = 0; column < Columns; column++)
+            {
+                if (grid[row, column] == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
+        public bool IsRowEmpty(int row)
+        {
+            for (int column = 0; column < Columns; column++)
+            {
+                if (grid[row, column] != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        private void ClearRow(int row)
+        {
+            for (int column = 0; column < Columns; column++)
+            {
+                grid[row, column] = 0;
+            }
+        }
+
+        private void MoveRowDown(int row, int numberOfRows)
+        {
+            for (int column = 0; column < Columns; column++)
+            {
+                grid[row + numberOfRows, column] = grid[row, column];
+                grid[row, column] = 0;
+            }
+        }
+
+        public int ClearFullRows()
+        {
+            int cleared = 0;
+            for (int row = Rows - 1; row >= 0; row--)
+            {
+                if (IsRowFull(row))
+                {
+                    ClearRow(row);
+                    cleared++;
+                }
+                else if (cleared > 0)
+                {
+                    MoveRowDown(row, cleared);
+                }
+            }
+
+            return cleared;
+        }
+    }
+}
